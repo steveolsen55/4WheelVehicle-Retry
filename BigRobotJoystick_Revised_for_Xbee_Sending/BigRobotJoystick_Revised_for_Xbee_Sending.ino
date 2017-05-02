d8e667356b76c68d4e0da21b2a672ff013fc709d
blakeway <l.blakeway1987@edmail.edcc.edu>
2017-05-01 21:41:36 -0700

    First revision

--- BigRobotJoystick_Revised_for_Xbee_Sending/BigRobotJoystick_Revised_for_Xbee_Sending.ino
+++ BigRobotJoystick_Revised_for_Xbee_Sending/BigRobotJoystick_Revised_for_Xbee_Sending.ino
@@ -1,105 +1,85 @@
 
- 
+
 const int MOTOR_VALUE_STOP = 0;
 
-const int X_JOYSTICK_MIN = 0;
-const int X_JOYSTICK_MAX = 1023;
-
-const int Y_JOYSTICK_MIN = 50;
-const int Y_JOYSTICK_MAX = 1023;
-
-const int SERIAL_COMMAND_SET_LEFT_MOTOR = 254;
-const int SERIAL_COMMAND_SET_RIGHT_MOTOR = 255;
-
-const int DEAD_ZONE = 15;
+const int SERIAL_COMMAND_SET_THROTTLE_MOTOR = 254;
+const int SERIAL_COMMAND_SET_STEERING_MOTOR = 255;
+const int SERIAL_COMMAND_SET_LEFT_BRAKE = 253;
+const int SERIAL_COMMAND_SET_RIGHT_BRAKE = 252;
 
 const unsigned long TIME_BETWEEN_GET_DATA = 100;
 const long SERIAL_DATA_SPEED_38400_BPS = 38400;
-     
- int JOYSTICK_X = A1;     
- int JOYSTICK_Y = A0; 
- 
- 
- int UDvalue;      
- int LRvalue;      
- int LeftServoVal;                 
- int RightServoVal;   
- 
- 
- unsigned long previousTime;
+
+int JOYSTICK_STEERING = A4;
+int JOYSTICK_THROTTLE = A5;
+int LEFT_BRAKE = A3;
+int RIGHT_BRAKE = A2;
+
+
+int throttleVal;
+int steeringVal;
+int leftBrakeVal;
+int rightBrakeVal;
+
+
+unsigned long previousTime;
 
 void setup()
 {
-    Serial.begin(SERIAL_DATA_SPEED_38400_BPS);
-         
-    previousTime = millis();
+  Serial.begin(SERIAL_DATA_SPEED_38400_BPS);
+
+  previousTime = millis();
 }
 
 void loop()
-{   
-      int UDvalue = 0;
-      int LRvalue = 0;
-      
-      if (millis()- previousTime >= TIME_BETWEEN_GET_DATA)
-      {
-        UDvalue = analogRead(JOYSTICK_Y);           
-        LRvalue = analogRead(JOYSTICK_X);   
-    
-        UDvalue = map(UDvalue,Y_JOYSTICK_MIN,Y_JOYSTICK_MAX,-100,100);  
-        LRvalue = map(LRvalue,X_JOYSTICK_MIN,X_JOYSTICK_MAX,-100,100);    
-        
-       if(UDvalue >= -DEAD_ZONE && UDvalue <= DEAD_ZONE && LRvalue <=  DEAD_ZONE && LRvalue >= -DEAD_ZONE) 
-       {           
-        
-          SendNewMotorValues(MOTOR_VALUE_STOP,MOTOR_VALUE_STOP);
-          previousTime = millis();
-        }
-        else                                   
-        {                                         
-          moveRobot(LRvalue,UDvalue);
-          previousTime = millis();
-        }
-      }
+{
+  int throttleVal = 0;
+  int steeringVal = 0;
+  int leftBrakeVal = 0;
+  int rightBrakeVal = 0;
+
+  if (millis() - previousTime >= TIME_BETWEEN_GET_DATA)
+  {
+    throttleVal = analogRead(JOYSTICK_THROTTLE);
+    steeringVal = analogRead(JOYSTICK_STEERING);
+    leftBrakeVal = analogRead(LEFT_BRAKE);
+    rightBrakeVal = analogRead(RIGHT_BRAKE);
+
+    throttleVal = constrain(throttleVal, 512, 1023);
+    throttleVal = map(throttleVal, 512, 1023, 0, 50);
+
+    steeringVal = map(steeringVal, 0, 180);
+
+    leftBrakeVal = map(leftBrakeVal, 0, 180);
+    rightBrakeVal = map(rightBrakeVal, 0, 180);
+
+    SendNewMotorValues(throttleVal, steeringVal, leftBrakeVal, rightBrakeVal);
+    previousTime = millis();
+  }
 }
-   
+}
+
 
 
 //****************************** SUBROUTINES ************************************
-  void SendNewMotorValues(char left, char right)
-      {
-        Serial.write (SERIAL_COMMAND_SET_LEFT_MOTOR);
-        Serial.write (left);
-                  
-        Serial.write (SERIAL_COMMAND_SET_RIGHT_MOTOR);
-        Serial.write (right);
-        Serial.print(left,DEC);
-        Serial.print("   ");
-        Serial.println(right,DEC);
-      }
-      
-//*************** Calculate motor values **************************************************      
-  void moveRobot (int valueX, int valueY)//subroutine to send pulses to the correct servo.
-  {
-    //Use the X direction to set the forward and backward motion
-    int leftMotorVal = valueY;
-    int rightMotorVal = valueY;
-    
-    //Apply the Y as a "twist" effect. (about 40% of the calculated value)
-    if (valueY >=0)
-    {
-      leftMotorVal = leftMotorVal + (valueX * 4)/10;
-      rightMotorVal = rightMotorVal - (valueX * 4)/10;
-    }
-    else
-    {
-      leftMotorVal = leftMotorVal - (valueX * 4)/10;
-      rightMotorVal = rightMotorVal + (valueX * 4)/10;
-    }
-      leftMotorVal = constrain(leftMotorVal,-100,100);
-      rightMotorVal =constrain(rightMotorVal,-100,100);
-     
-      SendNewMotorValues(leftMotorVal,rightMotorVal);
-  }
+void SendNewMotorValues(char thrust, char turn, char leftBrake, char rightBrake)
+{
+  Serial.write (SERIAL_COMMAND_SET_THROTTLE_MOTOR);
+  Serial.write (thrust);
+
+  Serial.write (SERIAL_COMMAND_SET_STEERING_MOTOR);
+  Serial.write (turn);
+
+  Serial.write (SERIAL_COMMAND_SET_LEFT_BRAKE);
+  Serial.write (leftBrake);
+
+  Serial.write (SERIAL_COMMAND_SET_RIGHT_BRAKE);
+  Serial.write (rightBrake);
+  
+  Serial.print(thrust, DEC);
+  Serial.print("   ");
+  Serial.println(turn, DEC);
+}
 
 
 
