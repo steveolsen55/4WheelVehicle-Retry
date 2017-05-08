b29f2b89d7cc86f6bf11048850647501f62a0841
mcmurraynick <mcmurraynick@yahoo.com>
2017-05-08 14:20:30 -0700

    Recieving Code

--- BigRobotJoystick_Revised_for_Xbee_Receiving/BigRobotJoystick_Revised_for_Xbee_Receiving.ino
+++ BigRobotJoystick_Revised_for_Xbee_Receiving/BigRobotJoystick_Revised_for_Xbee_Receiving.ino
@@ -7,15 +7,15 @@ Servo throttle;     ////////Is this a servo???
 
 const int LEFT_BRAKE_MOTOR = 8;
 const int RIGHT_BRAKE_MOTOR = 11;
-const int THROTTLE_MOTOR = 10;  ////////Will change once we wire///////
-const int STEERING_SERVO = 9;
+const int THROTTLE_MOTOR = 5;  ////////Will change once we wire///////
+const int STEERING_SERVO = 6;
 
 
 const int MOTOR_VALUE_OFF = 0;        ////////Test to make sure these values 
 const int MOTOR_VALUE_MAX = 179;      /////// will work (These are assuming 0
 const int MOTOR_VALUE_CENTER = 90;    /////// is no brakes and 179 is full brakes)
 
-const int NUMBER_OF_BYTES_IN_A_COMMAND = 4;
+const int NUMBER_OF_BYTES_IN_A_COMMAND = 8;
 const int SERIAL_COMMAND_SET_THROTTLE_MOTOR = 254;
 const int SERIAL_COMMAND_SET_STEERING_MOTOR = 255;
 const int SERIAL_COMMAND_SET_LEFT_BRAKE = 253;
@@ -46,32 +46,40 @@ void loop()
   static char leftBrakeVal = 0;
   static char throttleVal = 0;
   static char steeringVal = 90;
-  //Serial.println( Serial.available());
+  Serial.println( Serial.available());
   if (Serial.available() > NUMBER_OF_BYTES_IN_A_COMMAND)
   {
     int incomingByte = Serial.read();
-    Serial.println( incomingByte);
+//    Serial.print( incomingByte);
+    Serial.println("a");
     if (SERIAL_COMMAND_SET_LEFT_BRAKE == incomingByte)
     {
       leftBrakeVal = Serial.read();
-      
+//      Serial.print(leftBrakeVal);
+Serial.println("b");
     }
     if (SERIAL_COMMAND_SET_RIGHT_BRAKE == incomingByte)
     {
       rightBrakeVal = Serial.read();
+//      Serial.print(rightBrakeVal);
+      Serial.println("c");
     }
     if (SERIAL_COMMAND_SET_THROTTLE_MOTOR == incomingByte)
     {
       throttleVal = Serial.read();
+//      Serial.print(throttleVal);
+      Serial.println("d");
     }    
     if (SERIAL_COMMAND_SET_STEERING_MOTOR == incomingByte)
     {
       steeringVal = Serial.read();
+      Serial.println(steeringVal);
+      Serial.println("e");
     }
-    leftBrake.write(leftBrakeVal);
-    rightBrake.write(rightBrakeVal);
+    leftBrake.write(map(leftBrakeVal,0,100,0,179));
+    rightBrake.write(map(rightBrakeVal,0,100,0,179));
     throttle.write(throttleVal);
-    steering.write(steeringVal);
+    steering.write(map(steeringVal,0,100,0,179));
   }
 }
 
